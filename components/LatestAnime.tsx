86bfe1285d14b1f60a7bb44ab3b3eaf376510ec9
debarkamondal <debarkamondal@gmail.com>
2024-01-10 16:08:50 +0530

    latest anime section working

--- components/LatestAnime.tsx
+++ components/LatestAnime.tsx
@@ -1,5 +1,6 @@
 import { animeListType } from "@/app/utils/types";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type propType = {
@@ -12,13 +13,15 @@ const LatestAnime = (props: propType) => {
 			{latestAnime.map((anime) => {
 				return (
 					<div className="w-full p-2 md:w-44 shrink-0" key={anime.id}>
-						<Image
-							src={anime.image}
-							width={100}
-							height={200}
-							alt={anime.title.english}
-							className="rounded-md sm:h-64 md:h-56 w-full object-cover"
-						/>
+						<Link href={`/watch/${anime.id}`}>
+							<Image
+								src={anime.image}
+								width={100}
+								height={200}
+								alt={anime.title.english}
+								className="rounded-md sm:h-64 md:h-56 w-full object-cover"
+							/>
+						</Link>
 						<p className=" my-1 text-sm line-clamp-2 md:line-clamp-1 text-center">
 							{anime.title.english}
 						</p>
